9d672f77bb07a124782fdb7f81e48d3fd8c5710c
Christian Franke <chriss@frankeonline.net>
2013-09-10 23:19:06 +0200

    .bashrc.alias added + install.sh changed

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-ln -s nx-tools/.vimrc .vimrc
+
+if [ ! -L .vimrc ]; then 
+  ln -s nx-tools/.vimrc .vimrc
+fi
+
+grep ".bashrc.alias" .bashrc >> /dev/null || echo ". nx-tools/.bashrc.alias" >> .bashrc
